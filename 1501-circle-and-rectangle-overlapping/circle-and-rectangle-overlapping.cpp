eaa1fb3e38b64d4042864eebc0b7943c32f262af
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-01-21 18:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (17.35%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    bool checkOverlap(int r, int xC, int yC, int x1, int y1, int x2, int y2) {
-        // for x1 wala line
-        for(int y = y1;y <= y2;y++){
-            int val = (x1-xC)*(x1-xC) + (y-yC)*(y-yC);
-            if(val <= r*r) return true; 
+    bool checkOverlap(int r, int h, int k, int x1, int y1, int x2, int y2) {
+        int dx,dy;
+        if(h < x1){
+            dx = x1;
+
         }
-         // for x1 wala line
-        for(int y = y1;y <= y2;y++){
-            int val = (x2-xC)*(x2-xC) + (y-yC)*(y-yC);
-            if(val <= r*r) return true; 
+        else if(h > x2){
+            dx = x2;
+
         }
-         // for y1 wala line
-        for(int x = x1;x <= x2;x++){
-            int val = (x-xC)*(x-xC) + (y1-yC)*(y1-yC);
-            if(val <= r*r) return true; 
+        else {
+            dx = h;
         }
-         // for y2 wala line
-        for(int x = x1;x <= x2;x++){
-            int val = (x-xC)*(x-xC) + (y2-yC)*(y2-yC);
-            if(val <= r*r) return true; 
-        } 
 
-        if(xC < max(x1,x2) && yC < max(y1,y2) && xC > min(x1,x2)&& yC > min(y1,y2)) return true;
+         if(k < y1){
+             cout<<"idhar"<<endl;
+            dy = y1;
 
-        return false;       
-        
+        }
+        else if(k > y2){
+            cout<<"idhar1"<<endl;
+            dy = y2;
 
-
+        }
+        else {
+            cout<<"idhar2"<<endl;
+            dy = k;
+        }
+        cout<<dx<<endl;
+        cout<<dy<<endl;
+        cout<<(h-dx)<<endl;
+        cout<<(k-dy)<<endl;
+        return (h-dx) * (h-dx) + (k-dy)*(k-dy) <= (r*r);
     }
 };
