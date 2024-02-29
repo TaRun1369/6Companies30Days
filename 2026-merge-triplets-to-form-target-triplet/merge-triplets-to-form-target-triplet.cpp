38810bae0614953cfc7ab24e3c12827b2f99a2bd
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-02-29 12:15:18 +0530

    Time: 397 ms (41.81%) | Memory: 167.2 MB (16.95%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& trip, vector<int>& t) {
-        unordered_set<int> ind;
-        int c = 0;
+       
+        vector<int> final(3,INT_MIN);
         for(auto it : trip){
                 bool fl = false;
-                if(it[0] <= t[0] && it[1] <= t[1] && it[2] <= t[2]) fl = true;
-                if(fl == true) ind.insert(c);
-                c++;
+                if(it[0] <= t[0] && it[1] <= t[1] && it[2] <= t[2]){ fl = true;
+                final[0] = max(it[0],final[0]);
+                final[1] = max(it[1],final[1]);
+                final[2] = max(it[2],final[2]);
+                }
         }
-        vector<int> final(3,INT_MIN);
-        for(auto it : ind){
-            final[0] = max(trip[it][0],final[0]);
-            final[1] = max(trip[it][1],final[1]);
-            final[2] = max(trip[it][2],final[2]);
-        }
+        
+        // for(auto it : ind){
+            
+        // }
         if(final == t) return true;
         return false;
     }
