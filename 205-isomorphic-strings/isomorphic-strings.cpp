41a8899670deb3dc6c78728d420d6508559274ad
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-04-02 15:03:50 +0530

    Time: 6 ms (65.28%) | Memory: 8.2 MB (85.95%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.size();
+        vector<int> s1(256,-1),t1(256,-1);
+        for(int i = 0;i<n;i++){
+            if(s1[s[i]] == -1 && t1[t[i]] == -1){
+                s1[s[i]] = t[i];
+                t1[t[i]] = s[i];
+            }
+            else if((s1[s[i]] == -1 && t1[t[i]] != -1) || (t1[t[i]] == -1 && s1[s[i]] != -1)){
+                return false;
+            }
+            else{
+                if(s1[s[i]] != t[i] || t1[t[i]] != s[i]) return false;
+            }
+        }
+        return true;
+    }
+};
