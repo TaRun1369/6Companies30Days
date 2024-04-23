8880de0dac33e4913775803b94c39e88803ceb97
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-04-24 02:19:19 +0530

    Time: 120 ms (51.06%) | Memory: 59.8 MB (54.39%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,52 +1,39 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if (n == 1) {
-            return {0}; // Only one node, return the root
-        }
-        
-        unordered_map<int, vector<int>> graph;
-        vector<int> degrees(n, 0);
-        
-        for (const auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-            degrees[u]++;
-            degrees[v]++;
-        }
-        
+        if(n == 1) return {0};
+        vector<int> deg(n,0);
         queue<int> q;
-        for (int i = 0; i < n; i++) {
-            if (degrees[i] == 1) {
-                q.push(i);
-            }
+        vector<int> ans;
+        vector<vector<int>> adj(n);
+        for(auto it : edges){
+            adj[it[1]].push_back(it[0]);
+            adj[it[0]].push_back(it[1]);
+            deg[it[1]]++;
+            deg[it[0]]++;
         }
-        
-        int remainingNodes = n;
-        while (remainingNodes > 2) {
-            int size = q.size();
-            remainingNodes -= size;
-            
-            for (int i = 0; i < size; i++) {
-                int leaf = q.front();
+
+        for(int i = 0;i<n;i++){
+            if(deg[i] == 1) q.push(i);
+        }
+        while(n > 2){
+            int siz = q.size();
+            n -= siz;
+            while(siz--){
+                int f = q.front();
                 q.pop();
-                for (int neighbor : graph[leaf]) {
-                    degrees[neighbor]--;
-                    if (degrees[neighbor] == 1) {
-                        q.push(neighbor);
-                    }
+
+                for(auto it : adj[f]){
+                    deg[it]--;
+                    if(deg[it] == 1) q.push(it);
                 }
             }
+            
         }
-        
-        vector<int> result;
-        while (!q.empty()) {
-            result.push_back(q.front());
-            q.pop();
+
+        while(!q.empty()) {ans.push_back(q.front());
+        q.pop();
         }
-        
-        return result;
+        return ans;
     }
 };
