b4ed0f1847b82799c1d2b65ced8406112aa9187c
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-06-03 05:08:33 +0530

    Time: 3 ms (90.29%) | Memory: 11.9 MB (56.30%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        int n = nums.size();
-        for(int i = 0;i<32;i++){
-            int cnt = 0;
-            for(auto it : nums){
-                if(it & (1 << i))cnt++;
-                // cout<<it<<"     "<<cnt<<endl;
-            }
-            // cout<<i<<"      "<<cnt<<endl;
-            if(cnt % 3 == 1) ans = ans |  (1 << i);
-
-        }
-        return ans;
+       int ones= 0,twos = 0;
+       int n = nums.size();
+       for(int i = 0;i<n;i++){
+        ones = (ones^nums[i]) & (~twos);
+        twos = (twos^nums[i]) & (~ones);
+       }
+       return ones;
     }
 };
