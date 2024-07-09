a97571dbf820fb2cfbe8bda106fd51f799f432a1
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-07-09 23:40:10 +0530

    Time: 7 ms (63.96%) | Memory: 12 MB (55.02%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -9,8 +9,7 @@ public:
         if(i == 0){
             return j;
         }
-        if(w1[i - 1] == w2[j - 1]) return recur(i-1,j-1,w1,w2,dp);
-
+       
         if(dp[i][j] != -1) return dp[i][j];
 
         // insert 
@@ -20,14 +19,30 @@ public:
 
         return dp[i][j] = min({inse,dele,repla});
     }
-    int minDistance(string word1, string word2) {
-        if(word1 == word2) return 0;
-        if(word1.size() < word2.size()) return minDistance(word2,word1);
-        int n = word1.size(),m = word2.size();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        if(n == 0) return m;
-        if(m == 0) return n;
-        
-        return recur(n,m,word1,word2,dp);
+    int minDistance(string w1, string w2) {
+        // if(w1 == w2) return 0;
+        // if(w1.size() < w2.size()) return minDistance(w2,w1);
+        int n = w1.size(),m = w2.size();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        // if(n == 0) return m;
+        // if(m == 0) return n;
+        for(int i = 0;i<=n;i++) dp[i][0] = i;
+        for(int j = 0;j<=m;j++) dp[0][j] = j;
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                        
+                        if(w1[i - 1] == w2[j - 1])  dp[i][j] = dp[i -1][j-1];
+                        else{
+                        int inse =  dp[i][j-1];
+                        int dele =  dp[i - 1][j];
+                        int repla =  dp[i-1][j-1]; 
+
+                         dp[i][j] = 1 + min({inse,dele,repla});
+                        }
+            }
+        }
+        return dp[n][m];
+        // return recur(n,m,word1,word2,dp);
     }
 };
