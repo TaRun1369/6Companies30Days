19cc03569bc0874c2f8520f11ff01f60165023dc
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-07-09 23:44:15 +0530

    Time: 7 ms (63.96%) | Memory: 9.3 MB (95.64%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -23,26 +23,29 @@ public:
         // if(w1 == w2) return 0;
         // if(w1.size() < w2.size()) return minDistance(w2,w1);
         int n = w1.size(),m = w2.size();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        // vector<vector<int>> dp(n+1,vector<int> (m+1,0));
         // if(n == 0) return m;
         // if(m == 0) return n;
-        for(int i = 0;i<=n;i++) dp[i][0] = i;
-        for(int j = 0;j<=m;j++) dp[0][j] = j;
+        // for(int i = 0;i<=n;i++) dp[i][0] = i;
+        vector<int> prev(m+1,0),curr(m+1,0);
+        for(int j = 0;j<=m;j++) prev[j] = j;
 
         for(int i = 1;i<=n;i++){
+            curr[0] = i;
             for(int j = 1;j<=m;j++){
                         
-                        if(w1[i - 1] == w2[j - 1])  dp[i][j] = dp[i -1][j-1];
+                        if(w1[i - 1] == w2[j - 1])  curr[j] = prev[j-1];
                         else{
-                        int inse =  dp[i][j-1];
-                        int dele =  dp[i - 1][j];
-                        int repla =  dp[i-1][j-1]; 
+                        int inse =  curr[j-1];
+                        int dele =  prev[j];
+                        int repla =  prev[j-1]; 
 
-                         dp[i][j] = 1 + min({inse,dele,repla});
+                          curr[j] = 1 + min({inse,dele,repla});
                         }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
         // return recur(n,m,word1,word2,dp);
     }
 };
