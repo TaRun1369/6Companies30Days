6ea69209c02dd4cf62dca482f73aac26d4ea7fd0
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-07-11 16:46:59 +0530

    Time: 37 ms (86.59%) | Memory: 8 MB (90.14%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -12,7 +12,19 @@ int recur(int n){
     return dp[n] = mini;
 }
     int numSquares(int n) {
-        memset(dp,-1,sizeof(dp));
-        return recur(n);
+        memset(dp,0,sizeof(dp));
+        // for()
+        // return recur(n);
+        
+        for(int i = 1;i<=n;i++){
+                    int mini = INT_MAX;
+            for(int j = 1;j*j <= i;j++){
+                int ans = 1 + dp[i-j*j];
+                mini = min(ans,mini);
+            }
+            dp[i] = mini;
+        }
+        return dp[n];
+
     }
 };
